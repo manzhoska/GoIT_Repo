6e0c4fc0c34043a359d319343cda3d45776d455c
kateryna-manzhos <kateryna.manzhos@gmail.com>
2026-05-17 14:58:23 +0300

    added check for input values

--- GoIT Projects/Homeworks/goit-algo-hw-03/get_numbers_ticket.py
+++ GoIT Projects/Homeworks/goit-algo-hw-03/get_numbers_ticket.py
@@ -1,20 +1,24 @@
 import random
 
 def get_numbers_ticket(min, max, quantity):
-    # declare empty list 
-    lst = []
     # fill the list with the values one by one:
-    while len(lst) != quantity:
-        # generate random value
-        lst.append(random.randrange(min, max, quantity))
-        # making values unique in ht elist
-        lst = list(set(lst))
-        # sorting the list in ascending way
-        lst.sort()
-    return lst
+    if min >=1 and max <= 100 and max >= min and quantity <= (max - min):
+        # declare empty list 
+        lst = []
+        while len(lst) != quantity:
+            # generate random value
+            lst.append(random.randrange(min, max))
+            # making values unique in ht elist
+            lst = list(set(lst))
+            # sorting the list in ascending way
+            lst.sort()
+        return lst
+    else:
+        print("Please select positive integer numbers:\nmin >= 1 \nmax <= 100\nquantity <= (max - min)")
+    
 
 
-min = 1
+min = 10
 max = 100
 quantity = 6
-print("Ваші лотерейні числа:", get_numbers_ticket(min, max, quantity))
+print("Your lottery numbers:\t", get_numbers_ticket(min, max, quantity))
